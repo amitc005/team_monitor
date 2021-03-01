76ee123dd390427991e6b4227098b100ca07a77b
Amit Chavhan <achavahan@texadasoftware.com>
2021-02-28 19:59:09 -0500

    Added django user data set for testing purpose

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -4,5 +4,8 @@ echo "Apply Database migrations"
 python3 manage.py makemigrations --noinput
 python3 manage.py migrate
 
+echo "Loading test data"
+python manage.py loaddata data.json
+
 echo "Running team monitor server"
 python3 manage.py runserver 0.0.0.0:8000
